deae1531ddad878db7da581544bcd1e36addcdb9
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-09-19 12:09:59 +0300

    Use app brand from env for HTML title

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	envPrefix: "FRONTEND_",
+export default defineConfig(({ mode }) => {
+	const envPrefix = "FRONTEND_";
+	const env = loadEnv(mode, process.cwd(), envPrefix);
+
+	return {
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				minify: true,
+				inject: {
+					data: {
+						appBrand: env.FRONTEND_APP_BRAND,
+					},
+				},
+			}),
+		],
+		envPrefix: envPrefix,
+	};
 });
